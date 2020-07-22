d2b358bfeec8cee30adb4d275456e300dda2e4c5
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-22 16:42:09 -0700

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -21,3 +21,9 @@ class GetRequester
     end 
 
 end 
+
+
+# TESTING:
+
+# get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# get_requester.parse_json
