13d6bdb40a379d1b11df75332fcdd14dcd4a9f89
Joseph cohen <jojocohen57@gmail.com>
2020-07-24 02:31:45 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  def initialize(url)
+    @url = url
+  end 
+  
+  def get_response_body
+    uri = URI.parse(@url)
+    variable = Net::HTTP.get_response(uri)
+    variable.body
+  end
+  
+  def parse_json
+     JSON.parse(get_response_body)
+  end   
+end   
