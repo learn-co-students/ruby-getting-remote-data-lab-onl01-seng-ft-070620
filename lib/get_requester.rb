b403becbc0afe45b29f0905f1de095e37aba195e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 20:03:40 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
-# Write your code here
+require 'uri'
+require 'json'
+require 'pry'
+require 'net/http'
+
+class GetRequester
+
+  attr_accessor :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    people = JSON.parse(self.get_response_body)
+  end
+
+end
